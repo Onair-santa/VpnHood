38a9f8e14ad8eae45fea8e5363643a2bfc253548
Onair <42511409+Onair-santa@users.noreply.github.com>
2024-01-22 05:32:34 +0600

    Delete VpnHood.Client.App.Win directory

--- VpnHood.Client.App.Win/WinAppProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using VpnHood.Client.Device;
-using VpnHood.Client.Device.WinDivert;
-
-namespace VpnHood.Client.App;
-
-internal class WinAppProvider : IAppProvider
-{
-    public IDevice Device { get; } = new WinDivertDevice();
-}
